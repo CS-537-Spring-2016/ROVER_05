107528a21a6c7b9ef6befd8acf90df092bfdde82
pratikshah1701 <pratikshah1701@gmail.com>
2016-04-22 17:57:21 -0700

    updated the clases form location and Direction setategy

---
+++ src/rover05Tools/location/DirectionStrategy.java
@@ -0,0 +1,68 @@
+package rover05Tools.location;
+
+public class DirectionStrategy {
+
+	// based on the various situation decide on which direction to move on
+	// wisely
+
+	int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void moveNorth(int x, int y) {
+
+		//get the currect location 
+		LocationGetter loc=new LocationGetter();
+		int xCor=loc.getX();
+		int yCor=loc.getX();
+		
+		//sense the things using range extender and more decision
+		
+	}
+
+	public void moveSouth(int x, int y) {
+		
+		//get the currect location 
+		LocationGetter loc=new LocationGetter();
+		int xCor=loc.getX();
+		int yCor=loc.getX();
+		
+		//sense the things using range extender and more decision
+
+				
+
+	}
+
+	public void moveEast(int x, int y) {
+		
+		//get the currect location 
+		LocationGetter loc=new LocationGetter();
+		int xCor=loc.getX();
+		int yCor=loc.getX();
+		
+		//sense the things using range extender and more decision
+
+		
+		
+
+	}
+
+	public void moveWest(int x, int y) {
+		
+		//get the currect location 
+		LocationGetter loc=new LocationGetter();
+		int xCor=loc.getX();
+		int yCor=loc.getX();
+		
+		//sense the things using range extender and more decision
+
+
+	}
+
+}
---
+++ src/rover05Tools/location/LocationGetter.java
@@ -0,0 +1,42 @@
+package rover05Tools.location;
+
+public class LocationGetter {
+	
+	int x;
+	int y;
+	
+	
+	public LocationGetter(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	public LocationGetter() {
+	}
+
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+
+}
