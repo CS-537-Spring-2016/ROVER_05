413a4aef7df9756c02115347651ebe469a0937ee
dodialoufi <doaa7220370@gmail.com>
2016-05-10 13:59:36 -0700

    rover communication

--- src/common/Coord.java
+++ src/common/Coord.java
@@ -3,32 +3,21 @@ package common;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import enums.Science;
-import enums.Terrain;
-
 public class Coord {
-	public int xpos;
-	Terrain terrain;
-    boolean hasRover;
-    Science science;
-    
+	// thanks to this posting http://stackoverflow.com/questions/27581/what-issues-should-be-considered-when-overriding-equals-and-hashcode-in-java
+	
+	public final int xpos;
+	public final int ypos;
+	
 	@Override
 	public String toString() {
-	    return terrain + " " + science + " " + xpos + " " + ypos;
+		return "Coord [xpos=" + xpos + ", ypos=" + ypos + "]";
 	}
 
-	public int ypos;
-	
 	public Coord(int x, int y){
 		this.xpos = x;
 		this.ypos = y;
 	}
-	
-    public Coord(Terrain terrain, Science science, int x, int y) {
-        this(x, y);
-        this.science = science;
-        this.terrain = terrain;
-    }
 	
     @Override
     public int hashCode() {
@@ -46,12 +35,13 @@ public class Coord {
         if (obj == this)
             return true;
 
-        Coord other = (Coord) obj;
-        return new EqualsBuilder().
-            // if deriving: appendSuper(super.equals(obj)).
-            append(xpos, other.xpos).
-            append(ypos, other.ypos).
-            isEquals();
+        Coord theOther = (Coord) obj;
+//        return new EqualsBuilder().
+//            // if deriving: appendSuper(super.equals(obj)).
+//            append(xpos, theOther.xpos).
+//            append(ypos, theOther.ypos).
+//            isEquals();
+        return ((this.xpos == theOther.xpos) && (this.ypos == theOther.ypos));
     }
 	
 }
